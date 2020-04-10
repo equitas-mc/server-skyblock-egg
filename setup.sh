9cb0d110537788a5074b8f367f50e2ebeaf58cc1
Tobias Wilken <tooangel@tooangel.de>
2020-04-10 23:07:28 +0200

    Unify setup with US server
    - Add BlockInteractions
    - Use module properly
    - Skyblock as default world
    - Add autosave, but disabled because of error
    - Fix startup command in egg

--- setup.sh
+++ setup.sh
@@ -8,14 +8,13 @@ set -o xtrace
 apt update
 apt install -y wget sqlite3
 
-cd server-egg
-ls -al
-./module-cuberite-egg/setup.sh
+./server-egg/module-cuberite-egg/setup.sh
 
-cp settings.ini Cuberite/
-cp webadmin.ini Cuberite/
+cp server-egg/settings.ini Cuberite/
+cp server-egg/webadmin.ini Cuberite/
 
-ln -s ../../SkyBlock Cuberite/Plugins/SkyBlock
+ln -s ../../server-egg/SkyBlock Cuberite/Plugins/SkyBlock
+ln -s ../../server-egg/BlockInteractions Cuberite/Plugins/BlockInteractions
 
 # Update permissions, allow skyblock.command for default rank
 sqlite3 Cuberite/Ranks.sqlite "INSERT INTO PermissionItem (PermGroupID, permission) SELECT 1, 'skyblock.command' WHERE NOT EXISTS (SELECT 1 FROM PermissionItem WHERE PermGroupID = 1 AND permission = 'skyblock.command');"
