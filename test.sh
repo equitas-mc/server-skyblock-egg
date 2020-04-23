f52144d16f81a7cf4f255dd554768cbbf2b5fb8b
Tobias Wilken <tooangel@tooangel.de>
2020-04-23 22:07:39 +0200

    Improve `test.sh`, README and remove Ranks.sql
    The `Ranks.sql` is populated via the module.

--- test.sh
+++ test.sh
@@ -1,2 +1,4 @@
-docker build -t skyblock-server-egg .
-docker -D run --name server-skyblock-egg --rm --publish=25603:25603 --publish=26603:26603 -ti skyblock-server-egg:latest /bin/bash -c 'cd Cuberite && ./Cuberite'
+set -e
+
+docker build -t server-skyblock-egg .
+docker -D run --name server-skyblock-egg --rm --publish=25603:25603 --publish=26603:26603 -ti server-skyblock-egg:latest /bin/bash -c 'cd Cuberite && ./Cuberite'
